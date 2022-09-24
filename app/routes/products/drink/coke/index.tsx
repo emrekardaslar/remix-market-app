610026ae41dbab2d33d64fbdc88117ebaa71da7f
emrekardaslar <emre.kardaslar@gmail.com>
2022-09-24 14:59:30 +0300

    add sample loader

--- app/routes/products/drink/coke/index.tsx
+++ app/routes/products/drink/coke/index.tsx
@@ -1,13 +1,28 @@
-import { Link, Outlet } from '@remix-run/react'
+import { LoaderFunction } from '@remix-run/node';
+import { Link, Outlet, useLoaderData } from '@remix-run/react'
 import PageContent from '~/components/UI/PageContent'
 
+export const loader: LoaderFunction = async () => {
+  const data = {
+      items: [
+          { id: "1", name: "Coca-cola" },
+          { id: "2", name: "Pepsi" },
+      ]
+  }
+  return data;
+};
+
 function Coke() {
+  const {items} = useLoaderData();
   return (
     <>
         <PageContent>
           <h1>Coke</h1>
-          <Link to="./1">Coke 1</Link>
-          <Link to="./2">Coke 2</Link>
+          {items.map((item: any) => (
+            <>
+              <Link to={item.id}>{item.name} </Link>
+            </>
+          ))}
         </PageContent>
       <Outlet/>
     </>

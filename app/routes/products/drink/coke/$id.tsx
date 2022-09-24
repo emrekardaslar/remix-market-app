42621e4559301b866771f58c47e2c09eeffead44
emrekardaslar <emre.kardaslar@gmail.com>
2022-09-24 19:26:12 +0300

    add Card component

--- app/routes/products/drink/coke/$id.tsx
+++ app/routes/products/drink/coke/$id.tsx
@@ -1,24 +1,36 @@
 import { LoaderFunction } from '@remix-run/node';
-import { Outlet, useLoaderData, useParams } from '@remix-run/react'
+import { Outlet, useLoaderData } from '@remix-run/react'
+import { Card, Col, Divider, Rate, Row, Space } from 'antd';
+import Meta from 'antd/lib/card/Meta';
 import PageContent from '~/components/UI/PageContent';
 
 export const loader: LoaderFunction = async () => {
     const item = {
         id: 1,
         name: 'Coca-cola',
-        img: 'url'
+        img: 'https://image.influenster.com/eyJidWNrZXQiOiAiaW5mbHVlbnN0ZXJfcHJvZHVjdGlvbiIsICJrZXkiOiAibWVkaWEvcHJvZHVjdC9pbWFnZS9wcm9kdWN0L2ltYWdlL2RxdGxhanNmbnJ5cXlqNHduaWJmX0VCOGcxY0oucG5nIiwgImVkaXRzIjogeyJyb3RhdGUiOiBudWxsLCAicmVzaXplIjogeyJ3aWR0aCI6IDc1MCwgImhlaWdodCI6IDc1MCwgImZpdCI6ICJpbnNpZGUiLCAiYmFja2dyb3VuZCI6IHsiciI6IDEsICJnIjogMSwgImIiOiAxLCAiYWxwaGEiOiAwfSwgIndpdGhvdXRFbmxhcmdlbWVudCI6IHRydWV9fSwgImV4dGVuZCI6IHt9fQ=='
     }
     return item;
 };
 
 function CokeDetail() {
     const coke = useLoaderData()
-    const params = useParams();
+
     return (
         <div>
             <PageContent>
-                <h1>Coke: {coke.name}</h1>
-                <img src={coke.img}/>
+                <h1>{coke.name}</h1>
+                <Row gutter={60}>
+                  
+                        <Card
+                            style={{ width: 240 }}
+                            cover={<img alt="example" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqgT-aVvy9-AaxbhqALWPPBtzkmsi-1_z5Zw&usqp=CAU" />}
+                        >
+                            <Meta title={coke.name} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo veniam quo nesciunt amet expedita dignissimos error voluptas, distinctio saepe aut. Nemo assumenda temporibus vero aliquid! Recusandae provident laborum perferendis tempore?"} />
+                        </Card>
+                       
+                    <Meta title={coke.name} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo veniam quo nesciunt amet expedita dignissimos error voluptas, distinctio saepe aut. Nemo assumenda temporibus vero aliquid! Recusandae provident laborum perferendis tempore?"} />
+                </Row>
             </PageContent>
             <Outlet />
         </div>
--- app/routes/products/drink/coke/index.tsx
+++ app/routes/products/drink/coke/index.tsx
@@ -1,30 +1,43 @@
 import { LoaderFunction } from '@remix-run/node';
-import { Link, Outlet, useLoaderData } from '@remix-run/react'
+import { Link, Outlet, useLoaderData, useNavigate } from '@remix-run/react'
+import { Row, Col, Divider } from 'antd';
+import Card from 'antd/lib/card/Card';
+import Meta from 'antd/lib/card/Meta';
 import PageContent from '~/components/UI/PageContent'
 
 export const loader: LoaderFunction = async () => {
   const data = {
-      items: [
-          { id: "1", name: "Coca-cola" },
-          { id: "2", name: "Pepsi" },
-      ]
+    items: [
+      { id: "1", name: "Coca-cola", img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqgT-aVvy9-AaxbhqALWPPBtzkmsi-1_z5Zw&usqp=CAU', price: '10.99' },
+      { id: "2", name: "Pepsi", img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiByxQjdaBHgB0he4ip9G7w5nuYbbActS5pQ&usqp=CAU', price: '10.99' },
+    ]
   }
   return data;
 };
 
 function Coke() {
-  const {items} = useLoaderData();
+  const { items } = useLoaderData();
+  const navigate = useNavigate()
   return (
     <>
-        <PageContent>
-          <h1>Coke</h1>
+      <PageContent>
+        <h1>Coke</h1>
+        <Row gutter={16}>
           {items.map((item: any) => (
             <>
-              <Link key={item.id} to={item.id}>{item.name} </Link>
+              <div className="site-card-wrapper">
+                <Col span={6}>
+                  <Card hoverable size='small' title={item.name} bordered={false}
+                    style={{ width: 240 }} cover={<img alt="example" src={item.img} onClick={()=>navigate(`./${item.id}`)} />}>
+                    <Meta title={item.name} description={`Price: ${item.price}`} />
+                  </Card>
+                </Col>
+              </div>
             </>
           ))}
-        </PageContent>
-      <Outlet/>
+        </Row>
+      </PageContent>
+      <Outlet />
     </>
   )
 }

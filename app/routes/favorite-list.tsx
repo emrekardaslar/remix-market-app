87d2393d097017033ac44195f5bd53e9d2180bd3
emrekardaslar <emre.kardaslar@gmail.com>
2022-09-30 21:45:40 +0300

    add remix-auth module

--- app/routes/favorite-list.tsx
+++ app/routes/favorite-list.tsx
@@ -1,8 +1,21 @@
-import { Outlet } from "@remix-run/react"
+import { LoaderFunction, ActionFunction } from "@remix-run/node";
+import { Outlet, useLoaderData } from "@remix-run/react"
 import HeaderC from "~/components/Header"
+import authenticator from "~/services/auth.service";
 import headerItems from "../mock/headerItems"
 
+export let loader: LoaderFunction = async ({ request }) => {
+    return await authenticator.isAuthenticated(request, {
+        failureRedirect: "/login",
+    });
+};
+
+export const action: ActionFunction = async ({ request }) => {
+    await authenticator.logout(request, { redirectTo: "/login" });
+};
+
 function FavoriteList() {
+    const data = useLoaderData();
     return (
         <>
             <HeaderC items={headerItems} selectedKey='Favorite List' />
--- app/routes/orders.tsx
+++ app/routes/orders.tsx
@@ -1,6 +1,18 @@
+import { ActionFunction, LoaderFunction } from '@remix-run/node';
 import { Outlet } from '@remix-run/react'
 import HeaderC from '~/components/Header'
+import authenticator from '~/services/auth.service';
 import headerItems from "../mock/headerItems"
+
+export let loader: LoaderFunction = async ({ request }) => {
+  return await authenticator.isAuthenticated(request, {
+    failureRedirect: "/login",
+  });
+};
+
+export const action: ActionFunction = async ({ request }) => {
+  await authenticator.logout(request, { redirectTo: "/login" });
+};
 
 function Orders() {
   return (

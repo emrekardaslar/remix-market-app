9d0194d5a34246f0331cd9d97ebfe94cef55a20f
emrekardaslar <emre.kardaslar@gmail.com>
2022-10-09 22:43:59 +0300

    load tea data from server

--- app/routes/products/drink/tea/$id.tsx
+++ app/routes/products/drink/tea/$id.tsx
@@ -1,11 +1,36 @@
-import { useParams } from '@remix-run/react';
+import { json, LoaderFunction } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import { Row, Card } from 'antd';
+import Meta from 'antd/lib/card/Meta';
 import PageContent from '~/components/UI/PageContent';
+import { db } from '~/utils/db.server';
+
+export const loader: LoaderFunction = async ( {params} ) => {
+    return json(
+        await db.product.findFirst({
+            where: {
+                id: params.id
+            }
+        })
+    )
+}
 
 function TeaDetail() {
-    const params = useParams();
+    const tea = useLoaderData();
     return (
         <PageContent>
-            <h1>Water: {params.id}</h1>
+            <h1>{tea.name}</h1>
+            <Row gutter={60}>
+                  
+                  <Card
+                      style={{ width: 240 }}
+                      cover={<img alt={tea.name} src={tea.imgLink} />}
+                  >
+                      <Meta title={tea.name} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo veniam quo nesciunt amet expedita dignissimos error voluptas, distinctio saepe aut. Nemo assumenda temporibus vero aliquid! Recusandae provident laborum perferendis tempore?"} />
+                  </Card>
+                 
+              <Meta title={tea.name} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo veniam quo nesciunt amet expedita dignissimos error voluptas, distinctio saepe aut. Nemo assumenda temporibus vero aliquid! Recusandae provident laborum perferendis tempore?"} />
+          </Row>
         </PageContent>
     )
 }

3bf6543ffbd3ab23611a07799a2276e1fc7dc966
emrekardaslar <emre.kardaslar@gmail.com>
2022-11-12 10:23:54 +0300

    comment out items not being used

--- app/mock/sidebarItems.ts
+++ app/mock/sidebarItems.ts
@@ -1,7 +1,7 @@
 import { SidebarItem, SidebarMenu } from "../models/sidebarMenu"
 
 const snackItems: SidebarItem[] = [{name: 'Chocolate', subItems: []}, 
-{name: 'Ice Cream', subItems: []}, {name: 'Crisps', subItems: []}]
+/* {name: 'Ice Cream', subItems: []}, {name: 'Crisps', subItems: []} */]
 const drinkItems: SidebarItem[] = [{name: 'Coke', subItems: []}, {name: 'Water', subItems: []}, {name: 'Tea', subItems: []}]
 
 export const sidebarMenu: SidebarMenu = {

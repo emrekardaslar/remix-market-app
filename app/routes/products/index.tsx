b9a3c7fef163f9369e2b806d4754cdcbbfc658e4
emrekardaslar <emre.kardaslar@gmail.com>
2022-11-13 12:29:49 +0300

    add hpl to products page

--- app/routes/products/index.tsx
+++ app/routes/products/index.tsx
@@ -1,10 +1,50 @@
-import { Outlet } from "@remix-run/react"
+import { Product } from "@prisma/client"
+import { LoaderFunction } from "@remix-run/node"
+import { Outlet, useLoaderData, useNavigate } from "@remix-run/react"
+import { Hpl } from "emrekardaslar-uikit"
+import { db } from "~/utils/db.server"
 
+export let loader: LoaderFunction = async ({ request }) => {
+  let drink = await db.product.findMany(
+    {
+      where: {
+        category: 'drink'
+      }
+    }
+  );
+
+  let food = await db.product.findMany(
+    {
+      where: {
+        category: 'food'
+      },
+    }
+  );
+
+
+  return { drink, food }
+}
 
 function Products() {
+  const data = useLoaderData();
+  const keys = Object.keys(data);
+  const navigate = useNavigate();
+
+  const clickHandler = (product: Product) => {
+    navigate(`${product.category}/${product.subCategory}/${product.id}`)
+  }
+
   return (
     <>
-        <Outlet/>
+      {keys.map(key =>
+      (
+        <>
+          <h1 style={{ fontWeight: "bold", textTransform: "capitalize", marginLeft: "0.3rem" }}>{key}</h1>
+          <Hpl products={data[key]} onClick={clickHandler} button={true} />
+        </>
+      )
+      )}
+      <Outlet />
     </>
   )
 }

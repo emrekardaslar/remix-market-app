5c18fda70e597e43530991f1c6e547c1383bca0a
emrekardaslar <emre.kardaslar@gmail.com>
2023-02-25 12:54:26 +0300

    import hydrate directly

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,4 @@
+import { hydrate } from 'react-dom'
 import { RemixBrowser } from '@remix-run/react'
-import ReactDOM from 'react-dom'
 
-ReactDOM.hydrate(<RemixBrowser />, document)
+hydrate(<RemixBrowser />, document)
